451ecb1142f5749e29ff902caaa879ea7ae21eb1
Daniel Nielson <daniel@unfairsurprisery.org>
2017-11-06 14:58:49 -0700

    Accept a parameter in our awesome shortcode!

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ var App = require('ghost-app')
 MyAwesomeApp = App.extend({
   filters: {
     prePostsRender: 'handlePrePostsRender'
-  }
-, handlePrePostsRender: function handlePrePostsRender(payload) {
-    if (!payload.html) return payload;
+  },
+  handlePrePostsRender: function handlePrePostsRender(payload) {
+    if (Array.isArray(payload))
+      return payload.map(handlePrePostsRender);
+
     payload.html = payload.html.replace(
-      /\[awesome\]/g
-    , '<span style="color:darkorange;">Awesome!</span>'
+      /\[awesome( +\w+)?\]/g
+    , (match, param) => {
+        return '<span style="color:darkorange;">'
+        + (param ? param.trim() + ' is ' : '')
+        + 'Awesome!</span>'
+      }
     );
     return payload;
   }
