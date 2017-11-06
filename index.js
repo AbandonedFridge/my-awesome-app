b7f0b69c3a26d70fd79a483710ed7a315ffa5162
Daniel Nielson <daniel@unfairsurprisery.org>
2017-11-05 19:18:56 -0700

    Be anal about semi-colon placement.

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var App = require('ghost-app')
 MyAwesomeApp = App.extend({
   filters: {
     prePostsRender: 'handlePrePostsRender'
-  },
-  handlePrePostsRender: function handlePrePostsRender(payload) {
+  }
+, handlePrePostsRender: function handlePrePostsRender(payload) {
     if (!payload.html) return payload;
     payload.html = payload.html.replace(
       /\[awesome\]/g
